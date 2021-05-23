0cb850469da3e1be2b82b6a6475f9b02c06f5014
pablobrand <pabrandenburg@gmail.com>
2021-05-22 23:04:25 -0500

    contact form added

--- pages/contact.js
+++ pages/contact.js
@@ -1,35 +1,102 @@
-import { makeStyles } from '@material-ui/core/styles'
-import styles from '../styles/Home.module.css'
-import { Paper, Grid, TextField, Button} from '@material-ui/core'
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper, TextField, Button, Typography } from '@material-ui/core';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
 const useStyles = makeStyles((theme) => ({
-  myform: {
-    padding: theme.spacing(2),
+  formContainer: {
+    paddingTop:20,
+    maxWidth:'100%',
+    display: "flex",
+    justifyContent: "center",
+    
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.primary,
-    opacity: .9,
-    alignItems: 'static'
+  formBody:{
+    width: theme.spacing(100),
+    padding:5
   },
-}))
+  buttonForm: {    
+    display: "flex",
+    justifyContent: "center",
+  },
+  inputFieldDivider:{
+    padding:5
+  }
+}));
 
 export default function ContactForm () {
-  const classes = useStyles()
-  return ( 
-    <div className={styles.container}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={3} xs={12}>
-            <form className={classes.myform} noValidate autoComplete="off">
-                <TextField id="filled-basic" label="Full Name" variant="filled" />
-                <TextField id="filled-basic" label="Email" variant="filled" />
-                <TextField id="filled-basic" label="Message" variant="filled" />
-                <Button variant="contained" color="primary">
-                    Send Email
-                </Button>
-            </form>      
-        </Grid>
+  const classes = useStyles();
+  const [state, handleSubmit] = useForm("mdoyeqnp");
+  if (state.succeeded) {
+      return (
+        <div className={classes.formContainer}>
+          <Typography variant="h1" component="h2">
+            <DraftsIcon/>Thanks your for your messsage, we will get back to you as soon as possible
+          </Typography>
+        </div>
+  )}
+
+  return (
+    <div className={classes.formContainer}>
+      <Paper elevation={3}>
+        <form onSubmit={handleSubmit} className={classes.formBody}>
+          <div className={classes.inputFieldDivider}>
+            <TextField 
+              fullWidth 
+              id="email" 
+              label="Email Address" 
+              type="email" 
+              name="email" 
+              variant="filled"
+            />
+            {/* <label htmlFor="email">
+              Email Address
+            </label>
+            <input
+              id="email"
+              type="email" 
+              name="email"
+            /> */}
+            <ValidationError 
+              prefix="Email" 
+              field="email"
+              errors={state.errors}
+            />
+          </div>
+          <div className={classes.inputFieldDivider}>
+            {/* <textarea
+              id="message"
+              name="message"
+            /> */}
+            <TextField
+              fullWidth 
+              id="message"
+              label="Your Message"
+              name="message"              
+              multiline
+              rows={12}
+              variant="filled"
+            />
+            <ValidationError 
+              prefix="Message" 
+              field="message"
+              errors={state.errors}
+            />
+            </div>
+            <div className={classes.inputFieldDivider}>
+              <Button 
+                className={classes.buttonForm}
+                type="submit" 
+                disabled={state.submitting}
+                variant="contained" 
+                color="secondary"               
+                size="large"
+                >
+                  Send            
+              </Button>
+            </div>
+        </form>
       </Paper>
     </div>
   )}
