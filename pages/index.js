164abe4c740944c16ade8e0573c2090920b8f9ed
pablobrand <pabrandenburg@gmail.com>
2021-07-22 09:24:56 -0500

    update ga to work with nextjs

--- pages/index.js
+++ pages/index.js
@@ -50,14 +50,23 @@ export default function Home () {
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
 
-        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-203027768-1' />
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-203027768-1');
-        </script>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
+        />
       </Head>
       <Nav />
       <HomeSection />
